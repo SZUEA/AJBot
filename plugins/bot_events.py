e574c317ffd66f37d39278f6a5623915c322599b
frost <550991679@qq.com>
2022-10-18 12:31:02 +0800

    1

--- plugins/bot_events.py
+++ plugins/bot_events.py
@@ -11,6 +11,7 @@ from EAbotoy.model import WeChatMsg, EventMsg
 def receive_events(ctx: EventMsg):  # 函数名字只能是这个才能触发
     pass
     if ctx.EventName == EventNames.ON_EVENT_PAT_MSG and ctx.PattedUserName == ctx.CurrentWxid:
+        return
         Text("再拍我就拍死你[发怒]")
     elif ctx.EventName == EventNames.ON_EVENT_CHATROOM_INVITE_OTHER:
         Text(f"欢迎{ctx.data['InvitedNickName']} 加入，(^з^)")
